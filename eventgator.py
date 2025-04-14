0fa6edbc18e82c178f1d84a4a834000515483993
HariharanUmapathi <smarthariharan28@gmail.com>
2025-04-14 22:33:48 +0530

    chore: black code lint changes

--- eventgator.py
+++ eventgator.py
@@ -1,12 +1,11 @@
 import argparse
-import inspect
 import json
 
 from ICSGenerator import ICSGenerator
 from data_extractors.extractor_factory import ExtractorFactory
 
-""" 
-dynamic import not working so commented create a issue for discussion on it 
+"""
+dynamic import not working so commented create a issue for discussion on it
 import os,importlib
 data_extractors = 'dataextractors'
 for filename in os.listdir(data_extractors):
@@ -17,12 +16,14 @@ for filename in os.listdir(data_extractors):
         imported_module.append(module)
         #print(f"Imported {full_module_name}") """
 
-#defining Constants for various purposes
+# defining Constants for various purposes
 VERSION = "V 0.0.1"
 DESCRIPTION = f"""Event Aggregator {VERSION} 
-This Event Aggregator aggregates the events in specific format required for generating the ics file.
+This Event Aggregator aggregates the events in specific format required for 
+generating the ics file.
 """
- 
+
+
 class CLI(object):
 
     def __init__(self):
@@ -42,8 +43,8 @@ class CLI(object):
                 if source != '':
                     sources.add(source)
             except AttributeError as err:
-                print(f"{name} {err}")   
-        print("Current Event Sources ",sources)         
+                print(f"{name} {err}")  
+        print("Current Event Sources ", sources)       
         return sources
 
     def list_events(self):
@@ -63,29 +64,32 @@ class CLI(object):
 
     def generate_ics(self):
         print("Generating... ICS File")
-        with open("event_cache.json","r") as event_cache:
-            json_string  = event_cache.read()
+        with open("event_cache.json", "r") as event_cache:
+            json_string = event_cache.read()
             events = json.loads(json_string)
-            print("Events Available : ",len(events))
+            print("Events Available : ", len(events))
             print("writing events")
             icsgenerator = ICSGenerator()
-            icsgenerator.events_info=events
+            icsgenerator.events_info = events
             icsgenerator.write_ics_file()
             print("ICS File writing done")
- 
+
+
 def eventgator():
     cli = CLI()
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("-c","--command",dest="command",choices=cli.function_map.keys())
+    parser.add_argument("-c", "--command", dest="command",
+                        choices=cli.function_map.keys())
     args = parser.parse_args()
-    try: 
-        if args.command == None:
+    try:
+        if args.command is None:
             print("No Command Given")
             return parser.print_help()
         cli.function_map[args.command]()
     except KeyError as err:
-        print("Invalid Command",err)
+        print("Invalid Command", err)
         parser.print_help()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     eventgator()
